95191a63aad10a831e5add616e8f78fb9ee8c74a
FoxyStoat <carol.hodgson@hotmail.com>
2018-07-20 17:54:47 +0100

    feat: Add a route path to search and back to app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import * as BooksAPI from './BooksAPI';
 import './App.css';
 
-// import Search from './components/Search';
+import Search from './components/Search';
 import BookShelf from './components/BookShelf';
 
 class BooksApp extends React.Component {
@@ -26,31 +26,38 @@ class BooksApp extends React.Component {
     const { books } = this.state;
     return (
       <div className="app">
-        <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
+        <Route exact path="/" render={() => (
+          // render this when the path matches the url
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <BookShelf
+                shelfTitle="Curently Reading"
+                books={books}
+                shelf="currentlyReading"
+              />
+              <BookShelf
+                shelfTitle="Want To Read"
+                books={books}
+                shelf="wantToRead"
+              />
+              <BookShelf
+                shelfTitle="Read"
+                books={books}
+                shelf="read"
+              />
+            </div>
+						{/* Search Link */}
+            <div className="open-search">
+					    <Link to="/search">Add a book</Link>
+				    </div>
           </div>
-          <div className="list-books-content">
-            <BookShelf
-              shelfTitle="Curently Reading"
-              books={books}
-              shelf="currentlyReading"
-            />
-            <BookShelf
-              shelfTitle="Want To Read"
-              books={books}
-              shelf="wantToRead"
-            />
-            <BookShelf
-              shelfTitle="Read"
-              books={books}
-              shelf="read"
-            />
-          </div>
-        </div>
-        <div className="open-search">
-					<Link to="/search">Add a book</Link>
-				</div>
+				)}/> {/*route*/}
+
+				{/* If the url matches search render search component */}
+        <Route path='/search' component={Search}/>
       </div>
     )
   }
