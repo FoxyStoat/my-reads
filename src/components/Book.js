1571ab5368e28c877cd89b8f28aa6c374f0202b6
FoxyStoat <carol.hodgson@hotmail.com>
2018-07-20 11:20:16 +0100

    feat: Add small thumbnail image with an alt for each book

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,6 +4,7 @@ import BookShelfChanger from './BookShelfChanger';
 class Book extends React.Component {
   render() {
     const { book } = this.props;
+    console.log(book);
     return (
       <div className="book">
         <div className="book-top">
@@ -11,10 +12,14 @@ class Book extends React.Component {
             style={{
               width: 128,
               height: 192,
-              backgroundImage: `url(${book.imageLinks.thumbnail})`
-              }}>
+            }}>
+
+            <img className="book-cover-img" src={book.imageLinks.smallThumbnail}
+            alt={book.title}
+            />
+
           </div>
-          <BookShelfChanger />
+          <BookShelfChanger shelf={book.shelf} />
         </div>
         <div className="book-title">{book.title}</div>
         <div className="book-authors">{book.authors}</div>
